f5160faafea4bd35e9abe9a6b9b9d5b205e9fc44
Patrick <16281+paddyredbeard@users.noreply.github.com>
2023-08-19 17:53:32 -0700

    JSON & plaintext outputs

---
+++ handler.js
@@ -0,0 +1,28 @@
+'use-strict'
+
+module.exports.index = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        ip: event.requestContext.http.sourceIp,
+        utc: new Date()
+      },
+      null,
+      2
+    )
+  }
+}
+
+module.exports.plain = async (event) => {
+  return {
+    headers: {
+      'Content-Type': 'text/plain'
+    },
+    statusCode: 200,
+    body: '' + event.requestContext.http.sourceIp
+  }
+}
